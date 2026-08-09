7bd2a96f5f5c0166babc870d3be2a6af1c72ef30
Peccadotto <andrea.peccati@gmail.com>
2026-08-09 23:52:17 +0200

    Next commit

--- scripts/save_rds.py
+++ scripts/save_rds.py
@@ -7,6 +7,6 @@ def save_rds(dfs, output_dir = "output_rds"):
     
     for df_name, df in dfs.items():
         clean_name = df_name[3:] if df_name.startswith("df_") else df_name
-        out_file = os.path.join(output_dir, f"{clean_name}.rds")
+        out_file = os.path.join(output_dir, f"original_{clean_name}.rds")
         pyreadr.write_rds(out_file, df)
         print(f"[✓] File '{out_file}' created")
